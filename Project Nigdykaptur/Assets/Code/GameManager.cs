64dff8654d1ba05af8f3daa16a966f42a5bc590f
Figusorasu <99464311+Figusorasu@users.noreply.github.com>
2024-01-10 14:03:30 +0100

    v0.0.3
    - cursor changes on mouse hover over specific objects

---
+++ Project Nigdykaptur/Assets/Code/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } else {
+            Destroy(gameObject);
+            return;
+        };
+    }
+}
---
+++ Project Nigdykaptur/Assets/Code/ParticleDestoryer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestoryer : MonoBehaviour {
+    public float destroyTime;
+    void Start() {
+        StartCoroutine(DestroyerCo());
+    }
+    IEnumerator DestroyerCo() {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(this.gameObject);
+    }
+}
